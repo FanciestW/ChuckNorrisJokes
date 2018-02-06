92aed2ca931b421dc89e7452ec60291e99321293
William Lin <wlin26@yahoo.com>
2018-02-06 16:09:41 -0500

    Added function to get jokes on specific categories and get categories of jokes from api endpoint

--- ChuckNorrisJokes/joke.py
+++ ChuckNorrisJokes/joke.py
@@ -1,10 +1,44 @@
-import requests
+""" Chuck Norris Jokes Module """
+import sys
 import json
+import requests
+
+API_URL = 'https://api.chucknorris.io/jokes/'
 
 def main():
-    url = 'https://api.chucknorris.io/jokes/random'
-    resJSON = requests.get(url).json()
+    """ main function of program """
+    print(get_joke_categories())
+    # get_joke('dev')
+
+def get_joke(joke_category):
+    """ Get a Chuck Norris joke from a specific category """
+    parameters = {
+        "category" : joke_category
+    }
+    api_endpoint_string = API_URL + "random" + build_api_query(parameters)
+    try:
+        response = requests.get(api_endpoint_string)
+        response_json = response.json()
+        print(response_json['value'])
+    except:
+        print("Error", sys.exc_info()[0])
+        raise
+
+def build_api_query(query_parameters):
+    query_string = '?'
+    for key, value in query_parameters.items():
+        query_string += key + '=' + value + '&'
+    return query_string[:-1]
+
+def get_joke_categories():
+    """ Returns an array of joke categories """
+    try:
+        response = requests.get(API_URL + 'categories')
+        response_json = response.json()
+        return response_json
+    except:
+        print("Error", sys.exc_info()[0])
+        raise
     
-    print(resJSON)
 
 main()
